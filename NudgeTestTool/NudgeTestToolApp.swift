d1a4fec6fe621fa53f9949d002b491c2ab6c882e
George Babichev <gbabichev@xantrion.com>
2025-12-17 09:03:13 -0500

    Initial Commit

---
+++ NudgeTestTool/NudgeTestToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeTestToolApp.swift
+//  NudgeTestTool
+//
+//  Created by George Babichev on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct NudgeTestToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
